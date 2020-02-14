2f7004e595e92fbfacd8b4592572a7e8803b2853
e22tau <61043200+e22tau@users.noreply.github.com>
2020-02-14 15:18:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -6,6 +6,13 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'Fd4dxnIKjnlD69gh/j8QYMBV7VZa71a6nIbrlcAJtSwq4l930AsHlevXWJvxmKJtMPqfybe0R0VNRfJYc19E83AOzyQ1mgmRZfhCNwClZVmi4UBUweOEG89MMgddxtaE7LcuEHsCV3Bd4kw6v5JH8wdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '5bf01d5e0fbd686cdfc22a0e6d8979b7';
 $idPush = 'U416eaa460a7d41bdeb3c88dc81b0a4f2'
+	
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 // Get POST body content
 $content = file_get_contents('php://input');
